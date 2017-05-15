bbfbc859be223140a589234a5f619db905f8d2f9
hackwind <17989476@qq.com>
2017-05-15 17:23:57 +0800

    modify main page

--- app/src/main/java/com/tonyhu/cookbook/activity/AboutActivity.java
+++ app/src/main/java/com/tonyhu/cookbook/activity/AboutActivity.java
@@ -1,8 +1,10 @@
 
 package com.tonyhu.cookbook.activity;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.tonyhu.cookbook.R;
@@ -10,14 +12,14 @@ import com.tonyhu.cookbook.R;
 
 /*Created by Tony on 2017/3/29.
  */
-public class AboutActivity extends BaseActivity  {
+public class AboutActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.activity_about);
         getVersion();
-
+        findViewById(R.id.reward).setOnClickListener(this);
     }
 
     private void getVersion() {
@@ -32,5 +34,8 @@ public class AboutActivity extends BaseActivity  {
         }
     }
 
-
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this,RewardActivity.class));
+    }
 }
