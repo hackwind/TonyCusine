e649253895a7f57698f1da90ef68a05e9a737880
hackwind <17989476@qq.com>
2017-05-17 17:56:21 +0800

    modify welcome page

--- app/src/main/java/com/tonyhu/cookbook/activity/WelcomeActivity.java
+++ app/src/main/java/com/tonyhu/cookbook/activity/WelcomeActivity.java
@@ -12,55 +12,30 @@ import com.tonyhu.cookbook.R;
 import com.tonyhu.cookbook.fragment.WelcomeFragment;
 import com.tonyhu.cookbook.util.PreferenceUtil;
 
-public class WelcomeActivity extends com.blunderer.materialdesignlibrary.activities.ViewPagerActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstance) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstance);
-        firstLogin();
+        setContentView(R.layout.activity_welcome);
+        startTimer();
     }
 
-    @Override
-    protected ViewPagerHandler getViewPagerHandler() {
-        return new ViewPagerHandler()
-                .addPage(R.string.section1, WelcomeFragment.newInstance(R.drawable.welcome1,false))
-                .addPage(R.string.section1, WelcomeFragment.newInstance(R.drawable.welcome2,true));
-    }
-
-    @Override
-    protected boolean showViewPagerIndicator() {
-        return true;
-    }
-
-    @Override
-    protected boolean replaceActionBarTitleByViewPagerItemTitle() {
-        return true;
-    }
-
-    @Override
-    protected int defaultViewPagerItemSelectedPosition() {
-        return 0;
-    }
-
-    @Override
-    protected boolean hideToolbar() {
-        return true;
-    }
-
-    private void firstLogin() {
+    private void startTimer() {
         try {
-            PackageInfo info = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
-            String newVersion = info.versionName;
-            String oldVersion = PreferenceUtil.getString("version");
-            if(newVersion.equals(oldVersion)) {
-                Intent intent = new Intent(this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            } else {
-                PreferenceUtil.putString("version",newVersion);
-            }
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                  Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                  startActivity(intent);
+                    finish();
+                }
+            },2000);
         } catch (Exception e){
             e.printStackTrace();
         }
